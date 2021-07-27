500627a845b91786fb5c2bf05e4d071febdffcf3
Sixtus Innocent <sixtusinno@gmail.com>
2021-07-27 09:13:23 +0100

    removed NODE_ENV in frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,5 @@
 let apiUrl;
-if (process.env.NODE_ENV === "development") {
-  apiUrl = "http://localhost:5000/api";
-} else {
-  apiUrl = "https://bus-seat-reservation-api.herokuapp.com/api";
-}
+
+apiUrl = "https://bus-seat-reservation-api.herokuapp.com/api";
 
 export default apiUrl;
